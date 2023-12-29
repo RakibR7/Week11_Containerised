557b552a5c49a4178ffae60c69146a8b826ad136
RakibR7 <G00408644@ATU.IE>
2023-12-29 09:16:52 +0000

    base service 1

---
+++ src/main/java/atu/ie/week11_containerized/RegistrationServiceClient.java
@@ -0,0 +1,13 @@
+package atu.ie.week11_containerized;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@FeignClient(name = "confirmation-service", url = "http://localhost:8082")
+public interface RegistrationServiceClient {
+    @PostMapping("/confirm")
+    String somedetail(@RequestBody UserDetails userDetails);
+}
+
---
+++ src/main/java/atu/ie/week11_containerized/Week11ContainerizedApplication.java
@@ -0,0 +1,14 @@
+package atu.ie.week11_containerized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class Week11ContainerizedApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Week11ContainerizedApplication.class, args);
+	}
+
+}
